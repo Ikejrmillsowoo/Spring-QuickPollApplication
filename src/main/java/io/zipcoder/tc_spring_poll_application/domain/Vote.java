6f0bd58e80569d4846e3491a25c143f90b629f08
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-04-20 02:49:12 -0400

    vote class created

--- src/main/java/io/zipcoder/tc_spring_poll_application/domain/Vote.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Vote.java
@@ -1,4 +1,33 @@
 package io.zipcoder.tc_spring_poll_application.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Vote {
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Option getOption() {
+        return option;
+    }
+
+    public void setOption(Option option) {
+        this.option = option;
+    }
+
+    @Id
+    @GeneratedValue
+    @Column(name = "VOTE_ID")
+    private Long id;
+
+    @ManyToMany
+    @JoinColumn(name = "VOTE_ID")
+    private Option option;
+
 }
